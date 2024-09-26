6b602e7d38f520b27d3cd3b7024e268869954ee6
Rodrigo1 <email@ejemplo.es>
2024-09-26 20:23:21 +0200

    Caracteristica: implementar la suma.

--- Calculator.java
+++ Calculator.java
@@ -6,5 +6,13 @@ public class Calculator {
         // Codigo inicial que será ampliado por los desarrolladores
         System.out.println("Bienvenido a la Calculadora Básica.");
         scanner.close();
+        System.out.println("Ingrese el primer número para la suma:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Ingrese el segundo número para la suma:");
+        double num2 = scanner.nextDouble();
+
+        double result = num1 + num2;
+        System.out.println("El resultado de la suma es: " + resul
     }
 }
